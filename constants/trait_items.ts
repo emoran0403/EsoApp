b7510384f53b48a867a189bb27e4c970e3620181
Eric Moran <emoran0403@gmail.com>
2023-01-07 10:29:20 -0500

    added trait items, added todo list in gitignore

---
+++ constants/trait_items.ts
@@ -0,0 +1,39 @@
+export const WEAPON_TRAIT_ITEMS = {
+  Powered: 'Chysolite',
+  Charged: 'Amethyst',
+  Precise: 'Ruby',
+  Infused: 'Jade',
+  Defending: 'Turquoise',
+  Training: 'Carnelian',
+  Sharpened: 'Fire',
+  Decisive: 'Citrine',
+  Nirnhoned: 'Potent Nirncrux',
+};
+export const ARMOR_TRAIT_ITEMS = {
+  Sturdy: 'Quartz',
+  Impenetrable: 'Diamond',
+  Reinforced: 'Sardonyx',
+  Well_fitted: 'Almandine',
+  Training: 'Emerald',
+  Infused: 'Bloodstone',
+  Invigorating: 'Garnet',
+  Divines: 'Sapphire',
+  Nirnhoned: 'Fortified Nirncrux',
+};
+export const JEWELRY_TRAIT_ITEMS = {
+  Healthy: 'Antimony',
+  Arcane: 'Cobalt',
+  Robust: 'Zinc',
+  Bloodthirsty: 'Slaughterstone',
+  Harmony: 'Dibellium',
+  Infused: 'Aurbic Amber',
+  Triune: 'Dawn-Prism',
+  Protective: 'Titanium',
+  Swift: 'Gilding Wax',
+};
+
+export const TRAIT_ITEMS = {
+  WEAPONS: WEAPON_TRAIT_ITEMS,
+  ARMOR: ARMOR_TRAIT_ITEMS,
+  JEWELRY: JEWELRY_TRAIT_ITEMS,
+};
---
+++ constants/traits.ts
@@ -0,0 +1,41 @@
+export const WEAPON_TRAITS = [
+  'Powered',
+  'Charged',
+  'Precise',
+  'Infused',
+  'Defending',
+  'Training',
+  'Sharpened',
+  'Decisive',
+  'Nirnhoned',
+];
+
+export const ARMOR_TRAITS = [
+  'Sturdy',
+  'Impenetrable',
+  'Reinforced',
+  'Well-fitted',
+  'Training',
+  'Infused',
+  'Invigorating',
+  'Divines',
+  'Nirnhoned',
+];
+
+export const JEWELRY_TRAITS = [
+  'Healthy',
+  'Arcane',
+  'Robust',
+  'Bloodthirsty',
+  'Harmony',
+  'Infused',
+  'Triune',
+  'Protective',
+  'Swift',
+];
+
+export const All_TRAITS = {
+  WEAPONS: WEAPON_TRAITS,
+  ARMOR: ARMOR_TRAITS,
+  JEWELRY: JEWELRY_TRAITS,
+};
