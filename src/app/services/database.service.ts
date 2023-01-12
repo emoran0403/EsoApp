47ce69827fb6c9d07df3ecd1431cd1966113ae48
Eric Moran <emoran0403@gmail.com>
2023-01-12 09:39:13 -0500

    added placeholder query function to db service

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 
+export type query = 'GET' | 'POST' | 'PUT' | 'DELETE';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DatabaseService {
+  constructor() {}
 
-  constructor() { }
+  Query = (query: query, data: any) => {
+    switch (query) {
+      case 'GET':
+        // make a GET request to the db
+        break;
+      case 'POST':
+        // make a POST request to the db
+        break;
+      case 'PUT':
+        // make a PUT request to the db
+        break;
+      case 'DELETE':
+        // make a DELETE request to the db
+        break;
+    }
+  };
 }
