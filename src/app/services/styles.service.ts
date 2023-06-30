5557daf63f953f0beb168615113021c0a80878ab
Eric Moran <emoran0403@gmail.com>
2023-06-30 13:07:52 -0400

    added imports to services

--- src/app/services/styles.service.ts
+++ src/app/services/styles.service.ts
@@ -1,9 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StylesService {
-
-  constructor() { }
+  constructor() {}
 }
