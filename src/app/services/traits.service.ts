0e5b77ae562d3707eb011dec66b9cc7634ce46fd
Eric Moran <emoran0403@gmail.com>
2023-06-30 11:40:33 -0400

    generated services

---
+++ src/app/services/traits.service.ts
@@ -0,0 +1,10 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TraitsService {
+  constructor(private readonly http: HttpClient) {}
+}
