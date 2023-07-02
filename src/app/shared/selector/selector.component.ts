a841b07fcdaa48d9889baed2f3e16be756229105
Eric Moran <emoran0403@gmail.com>
2023-07-02 15:05:53 -0400

    switched to scss, added html skeleton

--- src/app/shared/selector/selector.component.ts
+++ src/app/shared/selector/selector.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-selector',
   templateUrl: './selector.component.html',
-  styleUrls: ['./selector.component.css']
+  styleUrls: ['./selector.component.scss'],
 })
 export class SelectorComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
