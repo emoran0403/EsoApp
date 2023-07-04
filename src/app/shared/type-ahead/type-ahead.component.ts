feee4e1d4c7a76600d1c4de82151fc42f7a0be7f
Eric Moran <emoran0403@gmail.com>
2023-07-04 09:39:41 -0400

    generated typeahead component for custom wrapper on ngx type ahead

---
+++ src/app/shared/type-ahead/type-ahead.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-type-ahead',
+  templateUrl: './type-ahead.component.html',
+  styleUrls: ['./type-ahead.component.scss'],
+})
+export class TypeAheadComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
