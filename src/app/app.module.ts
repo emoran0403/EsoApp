94b38de99f774af24bf0d503303e7de567a03b58
Eric Moran <emoran0403@gmail.com>
2023-07-04 10:34:41 -0400

    switched to ng-bootstrap over ngx-bootstrap and resolved node modules issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { WritsComponent } from './writs/writs.component';
@@ -13,13 +14,13 @@ import { PlayerWritsComponent } from './writs/player-writs/player-writs.componen
 import { WritCardComponent } from './writs/writ-card/writ-card.component';
 
 import { TraitsComponent } from './traits/traits.component';
+
 import { StylesComponent } from './styles/styles.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { UnlockButtonComponent } from './shared/unlock-button/unlock-button.component';
 import { SelectDropdownComponent } from './shared/select-dropdown/select-dropdown.component';
-
-import { CommonModule } from '@angular/common';
-
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TypeAheadComponent } from './shared/type-ahead/type-ahead.component';
 
 //! I should be able to wrap these within their parent module, but not sure how to do that yet
@@ -40,19 +41,19 @@ const PARENT_MODULES = [WritsComponent, TraitsComponent, StylesComponent];
     AppComponent,
     SelectDropdownComponent,
     UnlockButtonComponent,
+    TypeAheadComponent,
     ...PARENT_MODULES,
     ...CHILDREN_COMPONENTS,
     ...SHARED_COMPONENTS,
-    TypeAheadComponent,
   ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
-    BsDropdownModule,
     CommonModule,
     FormsModule,
     HttpClientModule,
+    NgbModule,
     ReactiveFormsModule,
   ],
   providers: [],
