9722f99158e085c2fa4e91a89677790233607708
Eric Moran <emoran0403@gmail.com>
2023-07-06 09:41:34 -0400

    added server urls to constants for services

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,6 +1,34 @@
+const BASE_URL = 'http://localhost:3000';
+const ENV = 'dev';
+const APP = 'esoapp';
+
 export const URLS = {
   traits: {
-    getAll: 'http://localhost:3000/dev/esoapp/traits/all',
-    updateOne: 'http://localhost:3000/dev/esoapp/traits/update',
+    getAll: `${BASE_URL}/${ENV}/${APP}/traits/all`,
+    updateOne: `${BASE_URL}/${ENV}/${APP}/traits/update`,
+    generate: `${BASE_URL}/${ENV}/${APP}/traits/generate`,
+    fill: `${BASE_URL}/${ENV}/${APP}/traits/fill`,
+  },
+  writs: {
+    getAll: `${BASE_URL}/${ENV}/${APP}/writs/all`,
+    updateOne: `${BASE_URL}/${ENV}/${APP}/writs/update`,
+    createOne: `${BASE_URL}/${ENV}/${APP}/writs/new`,
+    generate: `${BASE_URL}/${ENV}/${APP}/writs/generate`,
+  },
+  styles: {
+    getAll: `${BASE_URL}/${ENV}/${APP}/styles/all`,
+    getOne: `${BASE_URL}/${ENV}/${APP}/styles/one`,
+    updateOne: `${BASE_URL}/${ENV}/${APP}/styles/update`,
+    updateMany: `${BASE_URL}/${ENV}/${APP}/styles/update`,
+    addNew: `${BASE_URL}/${ENV}/${APP}/styles/new`,
+    generate: `${BASE_URL}/${ENV}/${APP}/styles/generate`,
+    fill: `${BASE_URL}/${ENV}/${APP}/styles/fill`,
+  },
+  auth: {
+    getOne: `${BASE_URL}/${ENV}/${APP}/users/one`,
+    getAll: `${BASE_URL}/${ENV}/${APP}/users/all`,
+    register: `${BASE_URL}/${ENV}/${APP}/users/new`,
+    login: `${BASE_URL}/${ENV}/${APP}/users/login`,
+    checkToken: `${BASE_URL}/${ENV}/${APP}/users/checkToken`,
   },
 };
--- src/app/services/writs.service.ts
+++ src/app/services/writs.service.ts
@@ -1,10 +1,21 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
+import { URLS } from './constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WritsService {
   constructor(private readonly http: HttpClient) {}
+
+  getAllWrits(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      player_uuid: '100',
+    });
+
+    return this.http.get(URLS.traits.getAll, { headers });
+  }
 }
