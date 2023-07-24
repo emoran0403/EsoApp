4292c4d75bbcc5be5b40bd907c64c1671cf69bcf
Eric Moran <emoran0403@gmail.com>
2023-07-24 09:12:37 -0400

    traits component updated with buttons to cycle thru crafting options

--- src/app/traits/traits.component.ts
+++ src/app/traits/traits.component.ts
@@ -36,14 +36,15 @@ export class TraitsComponent implements OnInit {
   readonly woodArmor = WOOD_APPAREL;
   readonly jeweleryItems = JEWELERY_ITEMS_ARR;
 
-  chosenCraftingType: string; // Crafting type which determines the items to show
-  shownItems; // The array of items to display and set traits on
-  chosenItem: all_items_list; // Item the user has chosen to toggle traits on
-  shownTraits: all_trait_options; // The traits that are shown based on chosenCraftingType
-  itemForButtons: [string, boolean][]; // chosenItem with the items traits on it
+  selectedCraftingType: string = 'Blacksmithing - Weapons'; // Crafting type which determines the items to show
+  craftingTypeIndex: number = 0;
+  shownItems: all_items_list[] = this.metalWeapons; // The array of items to display and set traits on
+  // chosenItem: all_items_list; // Item the user has chosen to toggle traits on
+  // shownTraits: all_trait_options; // The traits that are shown based on chosenCraftingType
+  // itemForButtons: [string, boolean][]; // chosenItem with the items traits on it
 
-  playerTraitItems: item[]; // Used to hold all traits from DB
-  filteredTraitItems: item[]; // playerTraits after filtering to match items in shownItems
+  // playerTraitItems: item[]; // Used to hold all traits from DB
+  // filteredTraitItems: item[]; // playerTraits after filtering to match items in shownItems
 
   craftingTypes: string[] = [
     'Blacksmithing - Weapons',
@@ -55,159 +56,64 @@ export class TraitsComponent implements OnInit {
   ];
 
   // Display Booleans
-  disableItems: boolean = true; // controls the disabling of the items dropdown
+  // disableItems: boolean = true; // controls the disabling of the items dropdown
 
-  ngOnInit(): void {
-    this.traits.getAllTraits().subscribe((data) => {
-      this.playerTraitItems = data;
-      console.log('this.playerTraits: ', this.playerTraitItems);
-    });
+  ngOnInit(): void {}
 
-    // setTimeout(() => {
-    //   console.log('this.playerTraits: ', this.playerTraits);
-    // }, 2000);
-  }
-
-  onAllTraitToggle(trait: any): void {
-    // console.log('All trait: ', trait);
-  }
-
-  /**
-   * Makes service call to set trait status in DB
-   * Updates playerTraitItems to reflect the new trait status
-   *
-   * @param item Tuple array containing the trait, and the new status of the trait
-   */
-  onSingleTraitToggle(item: [string, boolean]): void {
-    const [trait, value] = item;
-
-    // Convert trait to lowercase
-    const lowercasedTrait = trait.toLowerCase();
-
-    // Service call
-    this.traits
-      .updateOneTrait(this.chosenItem, trait, value)
-      .subscribe((res) => {
-        console.log('Service call response: ', res);
-
-        // Find the index of the item in playerTraitItems
-        const index = this.playerTraitItems.findIndex(
-          (item) => item.item.toLowerCase() === this.chosenItem.toLowerCase()
-        );
-
-        console.log('Item index: ', index);
-
-        if (index !== -1) {
-          // Create a copy of the item with the updated trait value
-          const updatedItem = {
-            ...this.playerTraitItems[index],
-            [lowercasedTrait]: value,
-          };
-
-          console.log('Updated item: ', updatedItem);
-
-          // Update the playerTraitItems array in place
-          this.playerTraitItems[index] = updatedItem;
-          console.log(
-            'UPDATED - this.playerTraitItems: ',
-            this.playerTraitItems
-          );
-        }
-
-        this.filterTraitItems();
-      });
-  }
-
-  /**
-   * Sets which kind of items are chosen
-   * @param type
-   */
-  handleCraftingTypeChange(type: any): void {
-    // console.log('crafting type chosen: ', type);
-    this.chosenCraftingType = type;
-    this.disableItems = false;
-    this.chosenItem = undefined; // This resets chosen item and effectively hides the traits
-
-    switch (type) {
-      case 'Blacksmithing - Weapons':
+  setShownItems(): void {
+    switch (this.craftingTypeIndex) {
+      case 0:
         this.shownItems = this.metalWeapons;
-        this.shownTraits = this.weaponTraits;
         break;
-      case 'Blacksmithing - Armor':
+      case 1:
         this.shownItems = this.metalArmor;
-        this.shownTraits = this.armorTraits;
         break;
-      case 'Woodworking - Weapons':
+      case 2:
         this.shownItems = this.woodWeapons;
-        this.shownTraits = this.weaponTraits;
         break;
-      case 'Woodworking - Armor':
+      case 3:
         this.shownItems = this.woodArmor;
-        this.shownTraits = this.armorTraits;
         break;
-      case 'Clothing':
+      case 4:
         this.shownItems = this.clothingArmor;
-        this.shownTraits = this.armorTraits;
         break;
-      case 'Jewelery':
+      case 5:
         this.shownItems = this.jeweleryItems;
-        this.shownTraits = this.jeweleryTraits;
         break;
     }
-
-    this.filterTraitItems();
   }
 
   /**
-   * Sets the item that was chosen
-   * @param item
+   * Helper function to set the selectedCraftingType based on the current craftingTypeIndex
    */
-  handleSelectedItemChange(item: string): void {
-    // console.log('item chosen: ', item);
-    this.chosenItem = item as all_items_list;
-    this.filterForButtons();
+  setCraftingTypeByIndex(): void {
+    this.selectedCraftingType = this.craftingTypes[`${this.craftingTypeIndex}`];
   }
 
   /**
-   * Filters down all items to just those that should be shown based on crafting type
+   * Sets craftingTypeIndex based on the given direction, loops back around from either direction.
+   * @param direction The direction along the craftingTypes array to travel.
    */
-  filterTraitItems(): void {
-    const lowercaseItems = this.data.lowercaseNoSpace(this.shownItems);
-
-    console.log('shownItems: ', lowercaseItems);
-
-    const filteredTraits = this.playerTraitItems.filter((obj) =>
-      lowercaseItems.includes(obj.item)
-    );
-
-    this.filteredTraitItems = [...filteredTraits];
-
-    console.log('filtered traits: ', this.filteredTraitItems);
-    console.log('chosenItem: ', this.chosenItem);
+  handleLooping(direction: 'next' | 'prev'): void {
+    if (direction === 'next') {
+      if (this.craftingTypeIndex + 1 > this.craftingTypes.length - 1) {
+        this.craftingTypeIndex = 0;
+      } else {
+        this.craftingTypeIndex++;
+      }
+    } else {
+      if (this.craftingTypeIndex - 1 < 0) {
+        this.craftingTypeIndex = this.craftingTypes.length - 1;
+      } else {
+        this.craftingTypeIndex--;
+      }
+    }
   }
 
-  /**
-   * Filters down the filteredTraitItems to a single item that was chosen, and prepares it for unlock buttons
-   */
-  filterForButtons(): void {
-    const filteredTraitItem = this.filteredTraitItems.filter((item: item) => {
-      return item.item === this.chosenItem.toLocaleLowerCase();
-    });
-
-    this.itemForButtons = this.data.makeTupleArray(filteredTraitItem[0]);
-
-    // console.log('filteredTraitItem: ', filteredTraitItem);
-    // console.log('this.itemForButtons: ', this.itemForButtons);
+  nextOrPrevCraftingType(direction: 'next' | 'prev'): void {
+    this.handleLooping(direction);
+    this.setCraftingTypeByIndex();
+    // this.getUnlocksForSelectedStyle(this.selectedStyle);
+    // this.scrollIntoView(this.selectedStyle);
   }
 }
-
-/**
- * DONE - make service call to get all traits
- * when the item is chosen - populate the trait buttons with db value
- * if toggle all is called - populate the trait buttons with value from toggle all button
- * what to do if first time user?
- *  - first time user should have falses everywhere
- *
- *
- *
- */
